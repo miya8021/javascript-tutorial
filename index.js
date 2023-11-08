82e6d300dcaed2568318fc8964af5b5baff36311
miya <miyagiyuka@Miya.local>
2023-11-09 06:52:36 +0900

    promise statusの状況を確認

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-function taskA() {
-    console.log("タスクAを実行 at " + Date.now());
-}
-function taskB() {
-    console.log("タスクBを実行 at " + Date.now());
-}
-function taskC() {
-    console.log("タスクCを実行 at " + Date.now());
-}
+//promise処理を追加
+// function taskA() {
+//     console.log("タスクAを実行 at " + Date.now());
+// }
+// function taskB() {
+//     console.log("タスクBを実行 at " + Date.now());
+// }
+// function taskC() {
+//     console.log("タスクCを実行 at " + Date.now());
+// }
 
-taskA();
-new Promise((resolve) => {
-  setTimeout(() => {
-      taskB();
-      resolve();
-  }, 1000);
-}).then(() =>{
-  taskC();
-})
+// taskA();
+// new Promise((resolve) => {
+//   setTimeout(() => {
+//       taskB();
+//       resolve();
+//   }, 1000);
+// }).then(() =>{
+//   taskC();
+// })
+
+//status resolveを実行
+// let promise = new Promise(function(resolve) {
+//   resolve()
+// }).then(()=>{
+//   console.log('resolve:成功');
+// });
+
+//status rejectを実行
+//catchにて実行される関数がreturnした値をresolveしている＝catchにて返されたpromiseオブジェクトのPromiseStatusはresolveになる
+let promise = new Promise(function(resolve, reject) {
+  reject()
+}).then(()=>{
+  console.log('resolve:成功');
+}).catch(()=>{
+  console.log('reject:失敗');
+});
